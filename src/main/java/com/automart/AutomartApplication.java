2d294e9142eb2478ad4aa870b148678e6d66d192
alexjime <alexji.dev@gmail.com>
2020-12-01 19:52:35 +0900

    start project

---
+++ src/main/java/com/automart/AutomartApplication.java
@@ -0,0 +1,13 @@
+package com.automart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automart/AutomartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
