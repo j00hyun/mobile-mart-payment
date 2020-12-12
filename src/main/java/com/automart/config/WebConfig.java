b134e050299cc637522bc00ecff882ca65fb90ae
j00hyun <loove1997@naver.com>
2020-12-13 04:08:26 +0900

    sms로직 추가

--- src/main/java/com/automart/config/WebConfig.java
+++ src/main/java/com/automart/config/WebConfig.java
@@ -45,8 +45,8 @@ public class WebConfig extends WebSecurityConfigurerAdapter { // Spring Security
         http
                 // 접근에 대한 인증 설정
                 .authorizeRequests()
-                .antMatchers("/login", "/signup", "/user").permitAll() // 누구나 접근 허용
-                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
+                .antMatchers("/login", "/signup").permitAll() // 누구나 접근 허용
+                .antMatchers("/").hasRole("USER") // USER만 접근 가능
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
--- src/main/java/com/automart/service/UserService.java
+++ src/main/java/com/automart/service/UserService.java
@@ -16,9 +16,23 @@ import java.util.Optional;
 public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
+    private SMSService smsService;
 
     /**
-     * 로컬 회원 이메일 확인
+     * Spring Security 필수 메소드
+     * 로컬 로그인
+     * @param email : 이메일
+     * @return : 이메일에 해당하는 유저 정보
+     */
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmailAndSnsType(email, "LOCAL")
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+
+    /**
+     * 이메일 중복확인
+     * @param email : 중복확인 요청하는 이메일 주소
      */
     public void checkDuplicateEmail(String email) throws ForbiddenSignUpException {
         log.info("이메일 중복 검증");
@@ -31,17 +45,8 @@ public class UserService implements UserDetailsService {
     }
 
     /**
-     * Spring Security 필수 메소드
-     * 로컬 로그인
-     */
-    @Override
-    public User loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmailAndSnsType(email, "LOCAL")
-                .orElseThrow(() -> new UsernameNotFoundException(email));
-    }
-
-    /**
-     * 중복 휴대폰 확인
+     * 휴대폰 중복확인
+     * @param tel : 중복확인 요청하는 휴대폰 번호
      */
     public void checkDuplicateTel(String tel) throws ForbiddenSignUpException {
         log.info("휴대폰 번호 중복 검증");
@@ -54,9 +59,60 @@ public class UserService implements UserDetailsService {
     }
 
     /**
-     * 회원가입
+     * 본인확인 인증번호 핸드폰으로 전송
+     * @param phone_no : 핸드폰 번호
+     * @return : 생성된 4자리 인증번호
+     */
+    public int validatePhone(String phone_no) {
+        log.info("인증번호 전송");
+
+        int valiNum = (int) (Math.random() * 100000);
+        String message = " [인증번호]\n" + valiNum;
+        smsService.sendMessage(phone_no, message);
+        return valiNum;
+    }
+
+
+    /**
+     * 임시 비밀번호 핸드폰으로 전송
+     * @param phone_no : 핸드폰 번호
+     * @return : 생성된 임시 비밀번호
+     */
+    public String generateTempPw(String phone_no) {
+        log.info("임시 비밀번호 전송");
+
+        String pw = "";
+        for (int i = 0; i < 12; i++) {
+            pw += (char) ((Math.random() * 26) + 97);
+        }
+        String message = "[임시 비밀번호]\n" + pw;
+        smsService.sendMessage(phone_no, message);
+        return pw;
+    }
+
+//    public int changePassword(int no, String password) {
+//        log.info("비밀번호 변겅");
+//
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        password = encoder.encode(password);
+//
+//        Optional<User> user = userRepository.findByNo(no);
+//
+//        if(user.isPresent()) {
+//        }
+//    }
+
+    /**
+     * 로컬 회원 생성
+     * @param email : 이메일
+     * @param password : 비밀번호
+     * @param name : 이름
+     * @param tel : 전화번호
+     * @return : 유저의 고유번호
      */
     public int saveUser(String email, String password, String name, String tel) {
+        log.info("로컬 회원 생성");
+
         // 입력받은 패스워드 암호화
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         password = encoder.encode(password);
