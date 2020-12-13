9e3bc567778d5e542897342fca1edef3ef2cb4f1
alexjime <alexji.dev@gmail.com>
2020-12-14 00:13:48 +0900

    add OrderController and Refactoring ProductController, OrderService, OrderServiceTest

--- src/main/java/com/automart/product/controller/ProductController.java
+++ src/main/java/com/automart/product/controller/ProductController.java
@@ -15,40 +15,41 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
 
     @ApiOperation("상품 등록")
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<ProductResponseDto> saveProduct(@RequestBody @Valid ProductSaveRequestDto requestDto) {
         ProductResponseDto productResponseDto = productService.saveProduct(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(productResponseDto);
     }
 
     @ApiOperation("상품 수정")
-    @PutMapping
+    @PutMapping("/edit/{productNo}")
     public ResponseEntity<ProductResponseDto> updateProduct(@RequestBody @Valid ProductUpdateRequestDto requestDto) {
         ProductResponseDto productResponseDto = productService.updateProduct(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
     @ApiOperation("상품 제거")
-    @DeleteMapping("/{productNo}")
-    public ResponseEntity<Void> removeProduct(@PathVariable int productNo) {
+    @DeleteMapping("/delete")
+    public ResponseEntity<Void> removeProduct(@RequestParam(value="productNo") int productNo) {
         productService.removeProduct(productNo);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @ApiOperation("상품 상세 조회")
-    @GetMapping("/{productNo}")
+    @GetMapping("/detail/{productNo}")
     public ResponseEntity<ProductResponseDto> showProduct(@PathVariable int productNo) {
         ProductResponseDto productResponseDto = productService.showProduct(productNo);
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
     }
 
     @ApiOperation("상품 목록 조회")
-    @GetMapping
+    @GetMapping("/list")
     public ResponseEntity<List<ProductResponseDto>> showProducts() {
         List<ProductResponseDto> productResponseDtos = productService.showProducts();
         return ResponseEntity.status(HttpStatus.OK).body(productResponseDtos);
