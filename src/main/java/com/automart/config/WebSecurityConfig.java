85dd93c3543afbc9b84f42c3e0f14b6a9f7f3197
j00hyun <loove1997@naver.com>
2020-12-25 00:11:40 +0900

    oauth2 기본설정

---
+++ src/main/java/com/automart/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.automart.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring에 OAuth2 Login 사용한다고 알림
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/oauth2/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
