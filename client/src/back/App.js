4f5d39fba2b493f25ab9828eb0874130d69801aa
jongsoo <soohappykjs7@naver.com>
2020-12-27 00:51:39 +0900

    init
    App.js - 단지 다른 인터페이스와 연결 목적 ex. user.js
    
    App.js.bak - mysql에서 데이터 가져오는 테스트 console에는 찍히는 데 <h3> {this.state.data} 이부분이 틀려서 못가져오는 듯하다.
    
    user.js - 제품상세(4번) 구현해보려 하는데 mysql 연동 안되어 있음
    한글 인코딩안되어 있음 utf-8 적용해야 할 듯 함
    chart(6번)는 https://www.wrappixel.com/templates/xtreme-react-admin-lite/
    에서 받음 (다른 프로젝트에 있음)

---
+++ client/src/back/App.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <header className="App-header">
+    <div className="App">
+        <meta charset="utf-8"/>
+	  <div className="App2">
+	  <a href="http://www.naver.com">
+	 hi  --   
+	  </a>
+	<a href="http://www.naver.com">
+	  hi2
+	  </a>
+	  </div>
+
+	  <div className="App2">
+	  <a href="http://www.naver.com">
+	 hi   
+	  </a>
+	  </div>
+
+    </div>
+      </header>
+  );
+}
+
+export default App;
